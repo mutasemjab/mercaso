77e93779c84dcff3fff308e453d8b6e9ba74be29
mutasem <mutasemms198@gmail.com>
2025-03-09 22:43:10 +0300

    banners

--- app/Http/Controllers/Api/v1/User/CategoryController.php
+++ app/Http/Controllers/Api/v1/User/CategoryController.php
@@ -23,20 +23,17 @@ class CategoryController extends Controller
     {
         $token = $request->bearerToken();
         $authenticatedUser = null;
-    
+
         if ($token) {
             $authenticatedUser = Auth::guard('user-api')->user();
         }
-    
+
         if ($authenticatedUser) {
-            // Get the country ID of the authenticated user
-            $countryId = $authenticatedUser->country_id;
-    
+
+
             // Retrieve categories that are associated with the user's country
             $categories = Category::whereNull('category_id')
-                ->whereHas('countries', function ($query) use ($countryId) {
-                    $query->where('country_id', $countryId);
-                })->with([
+                ->with([
                     'childCategories.products.productImages',
                     'childCategories.products.productReviews',
                     'childCategories.products.variations',
@@ -56,28 +53,24 @@ class CategoryController extends Controller
                     'childCategories.products.offers'
                 ])->get();
         }
-    
+
         // Format the response
         $response = $categories->map(function ($category) use ($authenticatedUser) {
             return [
                 'id' => $category->id,
                 'name' => $category->name,
                 'photo' => $category->photo,
-                'color' => $category->color,
-                'color_picker' => $category->color_picker,
                 'childCategories' => $category->childCategories->map(function ($childCategory) use ($authenticatedUser) {
                     return [
                         'id' => $childCategory->id,
                         'name' => $childCategory->name,
                         'photo' => $childCategory->photo,
-                        'color' => $childCategory->color,
-                        'color_picker' => $childCategory->color_picker,
                         'products' => $this->formatProducts($childCategory->products, $authenticatedUser)
                     ];
                 })
             ];
         });
-    
+
         return response()->json(['data' => $response]);
     }
 
@@ -85,58 +78,53 @@ class CategoryController extends Controller
     {
         $token = $request->bearerToken();
         $authenticatedUser = null;
-    
+
         if ($token) {
             $authenticatedUser = Auth::guard('user-api')->user();
         }
-    
+
         // Load the category with its child categories and their products
         $category = Category::with([
-            'childCategories.products.productImages', 
-            'childCategories.products.productReviews', 
-            'childCategories.products.variations', 
-            'childCategories.products.units', 
-            'childCategories.products.unit', 
-            'childCategories.products.offers', 
-            'products.productImages', 
-            'products.productReviews', 
-            'products.variations', 
-            'products.units', 
-            'products.unit', 
-            'products.offers', 
-            'countries'
+            'childCategories.products.productImages',
+            'childCategories.products.productReviews',
+            'childCategories.products.variations',
+            'childCategories.products.units',
+            'childCategories.products.unit',
+            'childCategories.products.offers',
+            'products.productImages',
+            'products.productReviews',
+            'products.variations',
+            'products.units',
+            'products.unit',
+            'products.offers',
         ])->find($id);
-    
+
         if (!$category) {
             return response()->json(['message' => 'Category not found'], 404);
         }
-    
+
         // Prepare the response data
         $response = [
             'category' => [
                 'name' => $category->name,
                 'photo' => $category->photo,
-                'color' => $category->color,
-                'color_picker' => $category->color_picker,
             ],
             'childCategories' => [],
             'products' => $this->formatProducts($category->products, $authenticatedUser)
         ];
-    
+
         foreach ($category->childCategories as $childCategory) {
             $childData = [
                 'category' => [
                     'name' => $childCategory->name,
                     'photo' => $childCategory->photo,
-                    'color' => $childCategory->color,
-                    'color_picker' => $childCategory->color_picker,
-                 
+
                 ],
                 'products' => $this->formatProducts($childCategory->products, $authenticatedUser)
             ];
             $response['childCategories'][] = $childData;
         }
-    
+
         return response()->json(['data' => $response]);
     }
 
@@ -184,7 +172,6 @@ class CategoryController extends Controller
         // Set additional product details
         $item->rating = $item->rating;
         $item->total_rating = $item->total_rating;
-        $item->currency = $item->category->countries->first()->sympol ?? '';
 
         $formattedProducts[] = $item;
     }
